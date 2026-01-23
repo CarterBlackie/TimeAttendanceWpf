13bec1b0472b6047fee8da7cd22581d2f0f81afe
Carter Blackie <carterblackie55@gmail.com>
2026-01-23 11:34:59 -0500

    Add select employee view code-behind

---
+++ TimeAttendanceWpf.UI/Views/SelectEmployeeView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace TimeAttendanceWpf.UI.Views;
+
+public partial class SelectEmployeeView : UserControl
+{
+    public SelectEmployeeView()
+    {
+        InitializeComponent();
+    }
+}
