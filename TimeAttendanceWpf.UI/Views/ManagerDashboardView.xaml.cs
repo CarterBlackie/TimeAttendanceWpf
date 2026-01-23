96855a096bda06db5dcc2f0deaadecb40bcb790f
Carter Blackie <carterblackie55@gmail.com>
2026-01-23 11:58:11 -0500

    Add manager dashboard view code-behind

---
+++ TimeAttendanceWpf.UI/Views/ManagerDashboardView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace TimeAttendanceWpf.UI.Views;
+
+public partial class ManagerDashboardView : UserControl
+{
+    public ManagerDashboardView()
+    {
+        InitializeComponent();
+    }
+}
