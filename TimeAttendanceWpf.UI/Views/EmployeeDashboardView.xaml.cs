55cd43b8875fb8710a26df771ad59a5c63fd8a59
Carter Blackie <carterblackie55@gmail.com>
2026-01-23 11:57:59 -0500

    Add employee dashboard view code-behind

---
+++ TimeAttendanceWpf.UI/Views/EmployeeDashboardView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace TimeAttendanceWpf.UI.Views;
+
+public partial class EmployeeDashboardView : UserControl
+{
+    public EmployeeDashboardView()
+    {
+        InitializeComponent();
+    }
+}
