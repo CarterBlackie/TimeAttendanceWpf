000c692deebadbaf66269200ba02ae77acfaeb35
Carter Blackie <carterblackie55@gmail.com>
2026-01-23 11:34:29 -0500

    Add navigation store for view model switching

---
+++ TimeAttendanceWpf.UI/ViewModels/NavigationStore.cs
@@ -0,0 +1,20 @@
+using TimeAttendanceWpf.UI.ViewModels;
+
+namespace TimeAttendanceWpf.UI.Services;
+
+public sealed class NavigationStore
+{
+    private ViewModelBase? _currentViewModel;
+
+    public ViewModelBase? CurrentViewModel
+    {
+        get => _currentViewModel;
+        set
+        {
+            _currentViewModel = value;
+            CurrentViewModelChanged?.Invoke();
+        }
+    }
+
+    public event Action? CurrentViewModelChanged;
+}
